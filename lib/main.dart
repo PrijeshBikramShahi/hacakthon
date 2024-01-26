6b9de05e9884f6590b96c24c61dc8989a645ee98
ronishg27 <iamrg2060@gmail.com>
2024-01-26 17:07:03 +0545

    editing on profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sequel_project/homepage.dart';
+// import 'package:sequel_project/homepage.dart';
+import 'package:sequel_project/screens/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
+      home: ProfilePage(),
     );
   }
 }
---
+++ lib/screens/profile.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Profile'),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text("Profile Page"),
+            Icon(Icons.account_circle, size: 120, color: Colors.deepOrange),
+            Row(
+              children: [
+                Text("Username: ",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Text("Prijesh Bikram Shahi"),
+              ],
+            ),
+            Row(
+              children: [
+                Text("Email: ", style: TextStyle(fontWeight: FontWeight.bold)),
+                Text("prijeshb@gmail.com"),
+              ],
+            ),
+          ],
+        ));
+  }
+}
