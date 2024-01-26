a8cf957585834786e583e803d003afb247fec24b
rYuma <prijeshshahi0@gmail.com>
2024-01-26 15:54:50 +0545

    start

---
+++ lib/homepage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(50, 50),
+        child: Flexible(
+          child: Container(),
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edit_document),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {}, label: Icon(Icons.add)),
+    );
+  }
+}
